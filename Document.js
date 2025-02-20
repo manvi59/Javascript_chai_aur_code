057dec61f48bf52fb8366a92adc04485a9f03837
manvi <manviyogi59@gmail.com>
2025-02-21 00:22:08 +0530

    DOM manupulation

---
+++ Document.js
@@ -0,0 +1,9 @@
+//  DOM--document object Model  -- it is interface of an HTML . which  represent the structure of the HTML / XML in tree format
+
+// The Document Object Model (DOM) is a programming interface that allows programs to change the structure, style, and content of a web page. It's a key part of making websites interactive.
+
+// by the helop of DOM we can modify web page uisng tree like strinure htML
+
+// ------------worked in inspact
+console.log(window); //global Object
+console.log(window.document); //provide the HTML content in tree format
