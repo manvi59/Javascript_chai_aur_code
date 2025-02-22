768e7944761447db20a5456aaf79d17a4c80194d
manvi <manviyogi59@gmail.com>
2025-02-22 13:43:36 +0530

    anscyncronous  setTimeout and setInterval function

--- Assynconyze.js
+++
@@ -1,6 +0,0 @@
-//  blocking code -----> (scrynonyze one by one )
-// non blocking code ----> (Asyncronyze  work like multi thread in between without waiting ) 
-
-// some time blocking code is reaquired to wait  like search user login data after user logeed in
-
-// sometime non blocking code is required in file operation .
